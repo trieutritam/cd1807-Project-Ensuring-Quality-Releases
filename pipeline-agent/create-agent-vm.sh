617f0d9d03df71ffe9baae6eab280fa8f26fc850
Tam Trieu Tri <tamtrieutri@gmail.com>
2024-05-19 14:42:44 +0700

    Change pipeline

--- pipeline-agent/create-agent-vm.sh
+++ pipeline-agent/create-agent-vm.sh
@@ -59,21 +59,6 @@ ENDSSH
 
       sudo snap install terraform --classic
 
-      sudo su root
-      # installs nvm (Node Version Manager)
-      curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-      # download and install Node.js
-      nvm install 20
-      # verifies the right Node.js version is in the environment
-      node -v # should print `v20.13.1`
-      # verifies the right NPM version is in the environment
-      npm -v # should print `10.5.2`
-
-      # Install newman
-      npm install -g newman
-
-      exit
-
       sudo reboot
 ENDSSH
 
