5d1d61deea2c3e7aecd44a067b2f9be4e435dd4b
Tam Trieu Tri <tamtrieutri@gmail.com>
2024-05-19 17:42:49 +0700

    Update terraform

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -17,7 +17,8 @@ resource "azurerm_windows_web_app" "test" {
   }
 
   site_config {
-    always_on = true
+    always_on = false
+    
     application_stack{
       current_stack = "dotnet"
       dotnet_version = "v4.0"
