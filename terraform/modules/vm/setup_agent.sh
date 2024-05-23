743b95de3dca67b8ab2239291d688142ac2211f4
Tam Trieu Tri <tamtrieutri@gmail.com>
2024-05-23 22:29:58 +0700

    Fix agent

--- terraform/modules/vm/setup_agent.sh
+++ terraform/modules/vm/setup_agent.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-sudo su tamtt5
-
 export AZ_DEVOPS_PAT="AZURE_PAT"
 
 # Copy the agent source provided by VM Image
-cp -R /home/packer/azagent ~/
-
-cd ~/azagent
+cp -R /home/packer/azagent /home/tamtt5
+chown -R tamtt5:tamtt5 /home/tamtt5/azagent
+cd /home/tamtt5/azagent
 
 # export AGENT_ALLOW_RUNASROOT="1"
 
 # Execute the file provided by VM Image to register Azure DevOps agent
-bash ./setup_devops_agent.sh
+sudo -H -u tamtt5 bash ./setup_devops_agent.sh
